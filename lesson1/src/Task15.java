4f728945935653cff38e5f19df1d770ae524a911
Dan Spînu <danuds98@gmail.com>
2020-09-11 13:20:27 +0300

    Chapter 2 Exercise 2.15

--- lesson1/src/Task15.java
+++ lesson1/src/Task15.java
@@ -1,6 +1,37 @@
-public class Task15 {
-    public static void main(String[] args) {
-        int num1;
-        int sum;
-    }
-}
+import java.util.Scanner;
+public class Task15
+{
+    // main method
+    public static void main(String[] args)
+    {
+        // Scanner to obtain integers
+        Scanner input=new Scanner(System.in);
+
+        // Initialize variable
+        int num1;	// first number
+        int num2;	// second number
+        int sum;	// sum of numbers
+        int product;	// product of numbers
+        int difference;	// difference of numbers
+        float quotient;	// quotient of numbers
+
+        System.out.print("Enter first number: ");
+        num1 = input.nextInt();	// first number input
+
+        System.out.print("Enter second integer: ");
+        num2 = input.nextInt();	// second number input
+
+        sum	= num1 + num2;	// sum numbers
+        product	= num1 * num2;	// multiply numbers
+        difference = num1 - num2;	// difference of numbers
+        quotient = (float)num1 / (float)num2;	// divide numbers
+
+        // display results
+        System.out.printf("Sum is %d\n", sum);
+        System.out.printf("Product is %d\n", product);
+        System.out.printf("Difference is %d\n", difference);
+        System.out.printf("Quotient is %.3f\n", quotient);
+
+    }	// end method main
+
+}   // end class
