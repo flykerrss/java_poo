c7407e881a4e5d7143010e6f31224d320b449b49
Dan Spînu <danuds98@gmail.com>
2020-09-19 10:42:54 +0300

    Chapter 6 Exercise 6.8

---
+++ lesson1/src/Task8.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Task8 {
+
+    //initialize variables
+    private static final double minimumFee = 2.00; //minimum fee (for 3 hours)
+    private static final double hourFee = 0.50; //fee for each number of hours greater than 3
+    private static final double maximumFee = 10.00; //maximum fee for 24hours
+    private static double totalCharges = 0.0f; //total charges for the day
+
+    //main method
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);   //scanner for data reading
+
+        double time;     //variable for time
+
+        do {
+            System.out.print("Enter number of hours: ");    //prompt
+            time = scanner.nextInt();
+            if (time > 0)     //check if number of hours is greater than 0
+                System.out.printf("Charges: $%.2f%n", calculatecharges(time));  //output charges
+
+        } while (time != 0);    //end of do-while cicle
+        System.out.printf("Total charges for the day: $%.2f%n", totalCharges);  //output total charges
+
+    }
+
+    //calculate charges method
+    public static double calculatecharges(double time) {
+        if (time > 3.0) {   //check if number of hours is greater than 3
+            double fee = minimumFee + (hourFee * (time - 3));    //calculate fee
+
+            totalCharges += Math.min(fee, maximumFee);  //calculate totalCharges
+            return Math.min(fee, maximumFee);   //return fee from current car
+        } else {    //else instructions
+            totalCharges += minimumFee; //calculate total charges
+            return minimumFee;      //return minimum fee
+        }
+    }   //end of main method
+}
