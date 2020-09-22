b1b8a3b56a3b03643235a9a9b83942b91a2ea726
Dan Spînu <danuds98@gmail.com>
2020-09-22 12:08:44 +0300

    Chapter 2 Exercise 2.28

--- lesson1/src/Task28.java
+++ lesson1/src/Task28.java
@@ -7,18 +7,11 @@ public class Task28 {
 
         System.out.print("Enter radius of circle: ");    //prompt
         int radius = scanner.nextInt(); //initialize radius
-        int diameter;   //declare diameter variable
-        double circumference; //declare circumference variable
-        double area;     //declare area variable
         double pi = 3.14159; //initialize pi variable
 
-        diameter = 2 * radius;          //calculate diameter
-        circumference = 2 * pi * radius;    //calculate circumference
-        area = pi * radius * radius;        //calculate area
-
-        System.out.println("Diameter: " + diameter);        //output result for diameter
-        System.out.println("Circumference: " + circumference);  //output result for circumference
-        System.out.println("Area: " + area);        //output result for area
+        System.out.println("Diameter: " + 2 * radius);        //output result for diameter
+        System.out.println("Circumference: " + 2 * pi * radius);  //output result for circumference
+        System.out.println("Area: " + pi * radius * radius);        //output result for area
 
     }   //end main method
 }
