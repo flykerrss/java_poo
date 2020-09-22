dc71fe66def4c836d4afb79a69610358b4800272
Dan Spînu <danuds98@gmail.com>
2020-09-22 12:38:05 +0300

    Chapter 2 Exercise 2.31

---
+++ lesson1/src/Task31.java
@@ -0,0 +1,8 @@
+public class Task31 {
+    //main method
+    public static void main(String[] args) {
+        System.out.println("number    square    cube");     //prompt
+        for (int number = 0; number < 10; number++)         //for cycle for calculate and output
+            System.out.printf("%d          %d         %d%n", number, number * number, number * number * number);
+    }       //end maint method
+}
